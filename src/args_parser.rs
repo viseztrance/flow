bc36f806c0fb9af1d7513adaffd27e1667a5bf97
Daniel Mircea <daniel@viseztrance.com>
2016-09-01 13:42:34 +0300

    Parse args via getops

---
+++ src/args_parser.rs
@@ -0,0 +1,71 @@
+use std::process;
+use getopts::{Options, Matches};
+
+pub struct ParsedArgs {
+    pub path_to_target_file: String,
+    pub path_to_config_file: String,
+    pub line_count: usize
+}
+
+pub struct ArgsParser {
+    args: Vec<String>,
+    options: Options
+}
+
+impl ArgsParser {
+    pub fn new(args: Vec<String>) -> ArgsParser {
+        let mut opts = Options::new();
+        opts.optopt("n", "lines", "Output the last NUM lines. Default is 10.", "NUM");
+        opts.optopt("c", "config", "Path to a config file. Defaults to looking in the current directory and user home.", "CONFIG");
+        opts.optflag("h", "help", "Print this help menu.");
+
+        ArgsParser {
+            args: args,
+            options: opts
+        }
+    }
+
+    pub fn process(&self) -> ParsedArgs {
+        let found_matches = self.options.parse(&self.args[1..]).unwrap();
+
+        if found_matches.opt_present("h") {
+            self.print_usage();
+            process::exit(0);
+        }
+
+        ParsedArgs {
+            path_to_target_file: self.get_target(&found_matches),
+            path_to_config_file: self.get_config(&found_matches),
+            line_count: self.get_lines(&found_matches)
+        }
+    }
+
+    fn print_usage(&self) {
+        let message = format!("Usage: {} FILE [options]", self.args[0]);
+        print!("{}", self.options.usage(&message));
+    }
+
+    fn get_target(&self, found_matches: &Matches) -> String {
+        match found_matches.free.get(0) {
+            Some(value) => value.to_string(),
+            None => {
+                self.print_usage();
+                process::exit(1);
+            }
+        }
+    }
+
+    fn get_config(&self, found_matches: &Matches) -> String {
+        match found_matches.opt_str("c") {
+            Some(value) => value.to_string(),
+            None => "some/path".to_string() // TODO: fallback to CWD and user home
+        }
+    }
+
+    fn get_lines(&self, found_matches: &Matches) -> usize {
+        match found_matches.opt_str("n") {
+            Some(value) => value.parse::<usize>().unwrap(),
+            None => 10
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate notify;
 extern crate ncurses;
+extern crate getopts;
 
 use std::env;
-use std::process;
 
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+use args_parser::ArgsParser;
 use flow::tail::Tail;
 use flow::flow::Flow;
 
+pub mod args_parser;
 pub mod flow {
     pub mod flow;
     pub mod tail;
@@ -18,22 +20,10 @@ pub mod flow {
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let args = env::args().collect();
 
-    let target = match args.get(1) {
-        Some(value) => value.to_string(),
-        None => {
-            println!("No file specified");
-            process::exit(0)
-        }
-    };
-
-    let line_count = match args.get(2) {
-        Some(value) => value.parse::<usize>().unwrap(),
-        None => 4
-    };
-
-    run(target, line_count);
+    let parsed_args = ArgsParser::new(args).process();
+    run(parsed_args.path_to_target_file, parsed_args.line_count);
 }
 
 fn run(target: String, line_count: usize) {
